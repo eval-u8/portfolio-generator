e8cfbda356fb1603f2d90affda23f63d7db2fc88
Ernesto Valecillos <evalecillos@gmail.com>
2021-09-30 22:17:22 -0500

    created app and template generator

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
+// console.log(profileDataArgs);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = (profileDataArr) => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = (profileDataArr) => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log("================");
+//     console.log("================");
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+// ------------------------------------------------------------------------------
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}.`;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err; //this creates an exception and stops the execution of the code. && any statements after `throw` won't be executed and control will be passed to the first `catch` block in the call. if theres no `catch` the program will terminate
+
+    console.log('Portfolio complete! Check out index.html to see the output');
+});
