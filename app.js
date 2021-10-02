7dd82872d9f7a76a40392c3052c2b155d7436cf0
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-01 23:29:45 -0500

    inquirer installed

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// ----------------------------------------------------------------
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+
+// ----------------------------------------------------------------
+
+// const pageHTML = generatePage(name, github);
+
+// const profileDataArgs = process.argv.slice(2);
+
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+
+// const [name, github] = profileDataArgs;
+
 // console.log(profileDataArgs);
 
 // // Notice the lack of parentheses around the `profileDataArr` parameter?
@@ -27,8 +49,11 @@ const [name, github] = profileDataArgs;
 
 // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}.`;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err; //this creates an exception and stops the execution of the code. && any statements after `throw` won't be executed and control will be passed to the first `catch` block in the call. if theres no `catch` the program will terminate
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err; //this creates an exception and stops the execution of the code. && any statements after `throw` won't be executed and control will be passed to the first `catch` block in the call. if theres no `catch` the program will terminate
 
-    console.log('Portfolio complete! Check out index.html to see the output');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output');
+// });
+
+
+
